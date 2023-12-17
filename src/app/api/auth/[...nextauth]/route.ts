cfa12c89f6f9e02d6c51ec0745d3b14a6b2cc6f3
Joshwa Facistol <fastline2123@gmail.com>
2023-12-17 23:37:07 +0800

    format next auto route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,47 +6,49 @@ import { connectDB } from '@/config/database';
 import Users from '@/models/users';
 
 const handler = NextAuth({
-	providers: [
-		GoogleProvider({
-			clientId: `${process.env.GOOGLE_CLIENT_ID}`,
-			clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-		}),
-	],
-	pages: {
-		signIn: '/login',
-	},
-	callbacks: {
-		async session({ session }: { session: any }) {
-			const sessionUser = await Users.findOne({
-				email_address: session.user.email,
-			});
+    providers: [
+        GoogleProvider({
+            clientId: `${process.env.GOOGLE_CLIENT_ID}`,
+            clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+        }),
+    ],
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        async session({ session }: { session: any }) {
+            const sessionUser = await Users.findOne({
+                email_address: session.user.email,
+            });
 
-			session.user.id = sessionUser._id.toString();
+            session.user.id = sessionUser._id.toString();
 
-			return session;
-		},
-		async signIn(data) {
-			try {
-				await connectDB();
+            return session;
+        },
+        async signIn(data) {
+            try {
+                await connectDB();
 
-				const user = await Users.findOne({
-					email_address: data.profile?.email,
-				});
+                const user = await Users.findOne({
+                    email_address: data.profile?.email,
+                });
 
-				if (!user) {
-					await Users.create({
-						email_address: data.profile?.email,
-						username: data.profile?.name?.replace(' ', '').toLowerCase(),
-					});
-				}
+                if (!user) {
+                    await Users.create({
+                        email_address: data.profile?.email,
+                        username: data.profile?.name
+                            ?.replace(' ', '')
+                            .toLowerCase(),
+                    });
+                }
 
-				return true;
-			} catch (error) {
-				console.error(error);
-				return false;
-			}
-		},
-	},
+                return true;
+            } catch (error) {
+                console.error(error);
+                return false;
+            }
+        },
+    },
 });
 
 export { handler as GET, handler as POST };
