2fa8e172b5a8b4fb5c9b39ee7c28a07f3dedc3f2
Joshwa Facistol <fastline2123@gmail.com>
2024-10-03 12:44:35 +0800

    removed image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 'use client';
 
 import { Box, Text, Container } from '@chakra-ui/react';
-import Image from 'next/image';
 
 const HomePage = () => {
     return (
         <Container maxW="container.md">
             <Box>
-                <Image
-                    src={'/next.svg'}
-                    alt="next logo"
-                    width={100}
-                    height={100}
-                />
                 <Text fontSize="5xl">Home Page</Text>
             </Box>
         </Container>
