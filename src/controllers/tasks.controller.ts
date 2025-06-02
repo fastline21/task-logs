863b996a387aa7485caaab8c42bfb0da975e0d1a
joshwa-work <135782700+joshwa-work@users.noreply.github.com>
2025-06-02 18:19:42 +0800

    Bugfix/build issue 6 (#7)
    * fix eslint issue
    
    * fixed string type

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -74,7 +74,7 @@ export const getAllTasksBySearchDate = async ({
 
 interface GetSingleTaskByIDInterface {
   payload: {
-    id: String;
+    id: string;
   };
 }
 export const getSingleTaskByID = async ({
